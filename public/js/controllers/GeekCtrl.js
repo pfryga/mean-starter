014a064212904b999c40e9914c3f7304f54f9d46
Piotr Fryga <piotr.fryga@allegro.pl>
2014-08-23 23:06:52 +0200

    repeat

--- public/js/controllers/GeekCtrl.js
+++ public/js/controllers/GeekCtrl.js
@@ -1,8 +1,6 @@
 angular.module('GeekCtrl', []).controller('GeekController', function($scope, Geek){
 	$scope.tagline = 'The square root of life is pi!';
 	Geek.get().success(function(data, status) {
-		console.log('wszedl do GET');
 		$scope.result = data;
-		console.log($scope.result);
 	});
 });
